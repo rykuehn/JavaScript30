308bdc5deee8640d7e4f991e38551d1541faf70f
Robin Kuehn <kuehn.robin@gmail.com>
2017-01-22 21:53:53 -0800

    create video permissions

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,26 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
+
+if (navigator.getUserMedia) {
+   navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
+      function(stream) {
+        console.log('success')
+         // var video = document.querySelector('video');
+         // video.srcObject = stream;
+         // video.onloadedmetadata = function(e) {
+         //   video.play();
+         // };
+      },
+      function(err) {
+         console.log("The following error occurred: " + err.name);
+      }
+   );
+} else {
+   console.log("getUserMedia not supported");
+}
